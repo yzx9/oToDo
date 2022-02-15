9f69bd3d81af95653f9ae396b565aa9e01cbbd6d
Celeste <yuan.zx@outlook.com>
2022-02-15 12:01:53 +0800

    fix(user): should not export password

--- entity/user.go
+++ entity/user.go
@@ -5,7 +5,7 @@ type User struct {
 
 	Name      string `json:"name" gorm:"size:128;uniqueIndex;"`
 	Nickname  string `json:"nickname" gorm:"size:128"`
-	Password  []byte `json:"password" gorm:"size:32;"`
+	Password  []byte `json:"-" gorm:"size:32;"`
 	Email     string `json:"email" gorm:"size:32;"`
 	Telephone string `json:"telephone" gorm:"size:16;"`
 	Avatar    string `json:"avatar"`
