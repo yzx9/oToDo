4e35036f4e92c1fafc1e6637022dea2997d6e01e
Celeste <yuan.zx@outlook.com>
2022-02-19 15:00:52 +0800

    refactor(todo): use scopes

--- dal/todo.go
+++ dal/todo.go
@@ -14,36 +14,32 @@ func InsertTodo(todo *entity.Todo) error {
 func SelectTodo(id int64) (entity.Todo, error) {
 	var todo entity.Todo
 	where := entity.Todo{Entity: entity.Entity{ID: id}}
-	re := getTodosWithPreload().Where(&where).First(&todo)
+	re := db.Scopes(todoPreload).Where(&where).First(&todo)
 	return todo, util.WrapGormErr(re.Error, "todo")
 }
 
 func SelectTodos(todoListID int64) ([]entity.Todo, error) {
 	var todos []entity.Todo
-	re := getTodosWithPreload().Where(entity.Todo{TodoListID: todoListID}).Find(&todos)
+	re := db.Scopes(todoPreload).Where(entity.Todo{TodoListID: todoListID}).Find(&todos)
 	return todos, util.WrapGormErr(re.Error, "todos")
 }
 
 func SelectImportantTodos(userID int64) ([]entity.Todo, error) {
 	var todos []entity.Todo
-	re := getTodosWithPreload().Where(entity.Todo{UserID: userID}).Where("Importance", true).Find(&todos)
+	re := db.Scopes(todoPreload).Where(entity.Todo{UserID: userID}).Where("Importance", true).Find(&todos)
 	return todos, util.WrapGormErr(re.Error, "important todos")
 }
 
 func SelectPlanedTodos(userID int64) ([]entity.Todo, error) {
 	var todos []entity.Todo
-	re := getTodosWithPreload().Where(entity.Todo{UserID: userID}).Not("deadline", nil).Order("deadline").Find(&todos)
+	re := db.Scopes(todoPreload).Where(entity.Todo{UserID: userID}).Not("deadline", nil).Order("deadline").Find(&todos)
 	return todos, util.WrapGormErr(re.Error, "planed todos")
 }
 
 func SelectNotNotifiedTodos(userID int64) ([]entity.Todo, error) {
 	var todos []entity.Todo
-	re := getTodosWithPreload().Where(entity.Todo{UserID: userID}).Not("notified", false).Order("notify_at").Find(&todos)
+	re := db.Scopes(todoPreload).Where(entity.Todo{UserID: userID}).Not("notified", false).Order("notify_at").Find(&todos)
 	return todos, util.WrapGormErr(re.Error, "not notified todos")
-}
-
-func getTodosWithPreload() *gorm.DB {
-	return db.Preload("Files").Preload("Steps").Preload("TodoRepeatPlan")
 }
 
 func SaveTodo(todo *entity.Todo) error {
@@ -63,4 +59,8 @@ func DeleteTodo(id int64) error {
 func DeleteTodos(todoListID int64) (int64, error) {
 	re := db.Where(entity.Todo{TodoListID: todoListID}).Delete(entity.Todo{})
 	return re.RowsAffected, util.WrapGormErr(re.Error, "todo")
+}
+
+func todoPreload(db *gorm.DB) *gorm.DB {
+	return db.Preload("Files").Preload("Steps").Preload("TodoRepeatPlan")
 }
