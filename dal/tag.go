9386520f0870599e6e908825c94b1a2ce0810d24
Celeste <yuan.zx@outlook.com>
2022-02-20 13:31:51 +0800

    perf(tag): create unique index

--- dal/tag.go
+++ dal/tag.go
@@ -3,6 +3,7 @@ package dal
 import (
 	"github.com/yzx9/otodo/model/entity"
 	"github.com/yzx9/otodo/util"
+	"gorm.io/gorm"
 )
 
 func InsertTag(tag *entity.Tag) error {
@@ -12,56 +13,51 @@ func InsertTag(tag *entity.Tag) error {
 
 func SelectTag(userID int64, tagName string) (entity.Tag, error) {
 	var tag entity.Tag
-	re := db.Where(&entity.Tag{
-		UserID: userID,
-		Name:   tagName,
-	}).First(&tag)
-	if re.Error != nil {
-		return entity.Tag{}, util.WrapGormErr(re.Error, "tag")
-	}
-
-	return tag, nil
+	re := db.Scopes(tagScope(userID, tagName)).First(&tag)
+	return tag, util.WrapGormErr(re.Error, "tag")
 }
 
 func SelectTags(userID int64) ([]entity.Tag, error) {
 	var tags []entity.Tag
-	re := db.Where(&entity.Tag{UserID: userID}).Find(&tags)
-	if re.Error != nil {
-		return nil, util.WrapGormErr(re.Error, "tag")
-	}
-
-	return tags, nil
+	re := db.Where(entity.Tag{UserID: userID}).Find(&tags)
+	return tags, util.WrapGormErr(re.Error, "tag")
 }
 
 func InsertTagTodo(userID, todoID int64, tagName string) error {
-	err := db.Model(&entity.Tag{}).Association("Todos").Append(&entity.Todo{
-		Entity: entity.Entity{
-			ID: todoID,
-		},
-	})
+	err := db.
+		Scopes(tagScope(userID, tagName)).
+		Association("Todos").
+		Append(&entity.Todo{Entity: entity.Entity{ID: todoID}})
 
 	return util.WrapGormErr(err, "tag todos")
 }
 
 func DeleteTagTodo(userID, todoID int64, tagName string) error {
-	err := db.Model(&entity.Tag{}).Association("Todos").Delete(&entity.Todo{
-		Entity: entity.Entity{
-			ID: todoID,
-		},
-	})
+	err := db.
+		Scopes(tagScope(userID, tagName)).
+		Association("Todos").
+		Delete(&entity.Todo{Entity: entity.Entity{ID: todoID}})
 
 	return util.WrapGormErr(err, "tag todos")
 }
 
 func ExistTag(userID int64, tagName string) (bool, error) {
 	var count int64
-	re := db.Model(&entity.Tag{}).Where(&entity.Tag{
-		UserID: userID,
-		Name:   tagName,
-	}).Count(&count)
+	re := db.Scopes(tagScope(userID, tagName)).Count(&count)
 	if re.Error != nil {
 		return false, util.WrapGormErr(re.Error, "tag")
 	}
 
 	return count != 0, nil
+}
+
+func tagScope(userID int64, tagName string) func(*gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		return db.
+			Model(&entity.Tag{}).
+			Where(&entity.Tag{
+				UserID: userID,
+				Name:   tagName,
+			})
+	}
 }
--- model/entity/tag.go
+++ model/entity/tag.go
@@ -3,9 +3,9 @@ package entity
 type Tag struct {
 	Entity
 
-	Name string `json:"name" gorm:"size:32"`
+	Name string `json:"name" gorm:"size:32;index:idx_tags_user,unique"`
 
-	UserID int64 `json:"userID"`
+	UserID int64 `json:"userID" gorm:"index:idx_tags_user,unique"`
 	User   User  `json:"-"`
 
 	Todos []Todo `json:"-" gorm:"many2many:tag_todos;"`
